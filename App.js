166c695a4793901e54814b379a82e52e1476c15e
Nataya Soledad <natayafs@gmail.com>
2021-03-28 00:26:54 -0300

    Create project app

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer, DrawerActions } from "@react-navigation/native";
+import HomeScreen from "../mobile-app/screens/HomeScreen";
+import RegisterScreen from "../mobile-app/screens/RegisterScreen";
+import SesionScreen from "../mobile-app/screens/SesionScreen";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+
+function CustomDrawerContent(props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Cerrar ventana"
+        onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
+      />
+    </DrawerContentScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function NotificationsScreen() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Bienvenida" component={HomeScreen} />
+      <Drawer.Screen name="Registro" component={RegisterScreen} />
+      <Drawer.Screen name="Inicio de sesión" component={SesionScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <NotificationsScreen />
+    </NavigationContainer>
+  );
+}
